7917feb78ffcd44ede9545d51dc5e8902bb2dbc1
Kevin <144486467+KQian-lab@users.noreply.github.com>
2024-04-02 14:07:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
+import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World from Kevin Qian in 3308'
+
+
+
+
+
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgres://lab_10_db_vxl3_user:N0Pzj8hQDKtJ8iaruo6lUrtrdNtbuej9@dpg-co669gq1hbls73b5f0r0-a/lab_10_db_vxl3")
+    conn.close()
+    return "Connection to DB successful"
